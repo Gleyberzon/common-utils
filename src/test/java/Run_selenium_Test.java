2fa5cda8b310fa86230180ffddf4bf692583a19d
Roman <roma0307.94@gmail.com>
2024-05-14 20:42:16 +0300

    Fix

--- src/test/java/Run_selenium_Test.java
+++ src/test/java/Run_selenium_Test.java
@@ -1,12 +1,15 @@
 
 import drivers.TesnetWebDriver;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
 import static systemUtils.SystemCommonUtils.sleep;
 
 public class Run_selenium_Test {
-    public static void main(String[] args) {
+
+    @Test
+    public void main_Test() {
         ChromeOptions options = new ChromeOptions();
         TesnetWebDriver tesnetWebDriver = new TesnetWebDriver(options);
 
